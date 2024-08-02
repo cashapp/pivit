c76bd27ba325187b891b01583a9a34a70e44976b
Yoav Amit <yoav@squareup.com>
2024-08-02 15:30:14 -0400

    rewrite the signing tests

--- pkg/pivit/sign_test.go
+++ pkg/pivit/sign_test.go
@@ -8,47 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSign_noFingerprint(t *testing.T) {
-	yk, err := testYubikey()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	patchPivVerify(yk)
-	defer unpatchPinVerify()
-
-	genOpts := &GenerateCertificateOpts{
-		Algorithm:   piv.AlgorithmEC384,
-		SelfSign:    true,
-		GenerateCsr: false,
-		AssumeYes:   true,
-		PINPolicy:   piv.PINPolicyNever,
-		TouchPolicy: piv.TouchPolicyAlways,
-		Slot:        piv.SlotCardAuthentication,
-		Prompt: &pinReader{
-			pin: piv.DefaultPIN + "\n",
-		},
-	}
-	err = GenerateCertificate(yk, genOpts)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	signOpts := &SignOpts{
-		StatusFd:           0,
-		Detach:             true,
-		Armor:              true,
-		UserId:             "NO MATCH",
-		TimestampAuthority: "",
-		Message:            &bytes.Buffer{},
-		Slot:               piv.SlotCardAuthentication,
-		Prompt:             nil,
-	}
-	err = Sign(yk, signOpts)
-	assert.Error(t, err)
-}
-
-func TestSign_happyPath(t *testing.T) {
+func TestSign(t *testing.T) {
 	yk, err := testYubikey()
 	if err != nil {
 		t.Fatal(err)
@@ -76,165 +36,98 @@ func TestSign_happyPath(t *testing.T) {
 
 	cert := yk.slots[piv.SlotCardAuthentication].cert
 	fingerprint := CertHexFingerprint(cert)
-	signOpts := &SignOpts{
-		StatusFd:           0,
-		Detach:             true,
-		Armor:              true,
-		UserId:             fingerprint,
-		TimestampAuthority: "http://timestamp.digicert.com",
-		Message:            &bytes.Buffer{},
-		Slot:               piv.SlotCardAuthentication,
-		Prompt:             nil,
-	}
-	err = Sign(yk, signOpts)
-	assert.NoError(t, err)
-}
 
-func TestSign_emptySlot(t *testing.T) {
-	yk, err := testYubikey()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	patchPivVerify(yk)
-	defer unpatchPinVerify()
-
-	signOpts := &SignOpts{
-		StatusFd:           0,
-		Detach:             true,
-		Armor:              true,
-		UserId:             "",
-		TimestampAuthority: "",
-		Message:            &bytes.Buffer{},
-		Slot:               piv.SlotCardAuthentication,
-		Prompt:             nil,
-	}
-	err = Sign(yk, signOpts)
-	assert.Error(t, err)
-}
-
-func TestSign_badTimestampServer(t *testing.T) {
-	yk, err := testYubikey()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	patchPivVerify(yk)
-	defer unpatchPinVerify()
-
-	genOpts := &GenerateCertificateOpts{
-		Algorithm:   piv.AlgorithmEC384,
-		SelfSign:    true,
-		GenerateCsr: false,
-		AssumeYes:   true,
-		PINPolicy:   piv.PINPolicyNever,
-		TouchPolicy: piv.TouchPolicyAlways,
-		Slot:        piv.SlotCardAuthentication,
-		Prompt: &pinReader{
-			pin: piv.DefaultPIN + "\n",
+	testCases := []struct {
+		description  string
+		armor        bool
+		detach       bool
+		userId       string
+		timestampUrl string
+		slot         piv.Slot
+		expectError  bool
+	}{
+		{
+			description:  "no certificate found",
+			armor:        false,
+			detach:       false,
+			userId:       "",
+			timestampUrl: "",
+			slot:         piv.SlotSignature,
+			expectError:  true,
+		},
+		{
+			description:  "bad fingerprint",
+			armor:        false,
+			detach:       false,
+			userId:       "",
+			timestampUrl: "",
+			slot:         piv.SlotCardAuthentication,
+			expectError:  true,
+		},
+		{
+			description:  "default options",
+			armor:        false,
+			detach:       false,
+			userId:       fingerprint,
+			timestampUrl: "",
+			slot:         piv.SlotCardAuthentication,
+			expectError:  false,
+		},
+		{
+			description:  "with armor",
+			armor:        true,
+			detach:       false,
+			userId:       fingerprint,
+			timestampUrl: "",
+			slot:         piv.SlotCardAuthentication,
+			expectError:  false,
+		},
+		{
+			description:  "detached signature",
+			armor:        false,
+			detach:       true,
+			userId:       fingerprint,
+			timestampUrl: "",
+			slot:         piv.SlotCardAuthentication,
+			expectError:  false,
+		},
+		{
+			description:  "with timestamp server",
+			armor:        false,
+			detach:       false,
+			userId:       fingerprint,
+			timestampUrl: "http://timestamp.digicert.com",
+			slot:         piv.SlotCardAuthentication,
+			expectError:  false,
+		},
+		{
+			description:  "bad timestamp server",
+			armor:        false,
+			detach:       false,
+			userId:       fingerprint,
+			timestampUrl: "http://127.0.0.1",
+			slot:         piv.SlotCardAuthentication,
+			expectError:  true,
 		},
 	}
-	err = GenerateCertificate(yk, genOpts)
-	if err != nil {
-		t.Fatal(err)
+	for _, test := range testCases {
+		t.Run(test.description, func(t *testing.T) {
+			signOpts := &SignOpts{
+				StatusFd:           0,
+				Detach:             test.detach,
+				Armor:              test.armor,
+				UserId:             test.userId,
+				TimestampAuthority: test.timestampUrl,
+				Message:            &bytes.Buffer{},
+				Slot:               test.slot,
+				Prompt:             nil,
+			}
+			err = Sign(yk, signOpts)
+			if test.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
 	}
-
-	cert := yk.slots[piv.SlotCardAuthentication].cert
-	fingerprint := CertHexFingerprint(cert)
-	signOpts := &SignOpts{
-		StatusFd:           0,
-		Detach:             true,
-		Armor:              true,
-		UserId:             fingerprint,
-		TimestampAuthority: "http://127.0.0.1",
-		Message:            &bytes.Buffer{},
-		Slot:               piv.SlotCardAuthentication,
-		Prompt:             nil,
-	}
-	err = Sign(yk, signOpts)
-	assert.Error(t, err)
-}
-
-func TestSign_attached(t *testing.T) {
-	yk, err := testYubikey()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	patchPivVerify(yk)
-	defer unpatchPinVerify()
-
-	genOpts := &GenerateCertificateOpts{
-		Algorithm:   piv.AlgorithmEC384,
-		SelfSign:    true,
-		GenerateCsr: false,
-		AssumeYes:   true,
-		PINPolicy:   piv.PINPolicyNever,
-		TouchPolicy: piv.TouchPolicyAlways,
-		Slot:        piv.SlotCardAuthentication,
-		Prompt: &pinReader{
-			pin: piv.DefaultPIN + "\n",
-		},
-	}
-	err = GenerateCertificate(yk, genOpts)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cert := yk.slots[piv.SlotCardAuthentication].cert
-	fingerprint := CertHexFingerprint(cert)
-	signOpts := &SignOpts{
-		StatusFd:           0,
-		Detach:             false,
-		Armor:              true,
-		UserId:             fingerprint,
-		TimestampAuthority: "",
-		Message:            &bytes.Buffer{},
-		Slot:               piv.SlotCardAuthentication,
-		Prompt:             nil,
-	}
-	err = Sign(yk, signOpts)
-	assert.NoError(t, err)
-}
-
-func TestSign_unarmored(t *testing.T) {
-	yk, err := testYubikey()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	patchPivVerify(yk)
-	defer unpatchPinVerify()
-
-	genOpts := &GenerateCertificateOpts{
-		Algorithm:   piv.AlgorithmEC384,
-		SelfSign:    true,
-		GenerateCsr: false,
-		AssumeYes:   true,
-		PINPolicy:   piv.PINPolicyNever,
-		TouchPolicy: piv.TouchPolicyAlways,
-		Slot:        piv.SlotCardAuthentication,
-		Prompt: &pinReader{
-			pin: piv.DefaultPIN + "\n",
-		},
-	}
-	err = GenerateCertificate(yk, genOpts)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cert := yk.slots[piv.SlotCardAuthentication].cert
-	fingerprint := CertHexFingerprint(cert)
-	signOpts := &SignOpts{
-		StatusFd:           0,
-		Detach:             true,
-		Armor:              false,
-		UserId:             fingerprint,
-		TimestampAuthority: "",
-		Message:            &bytes.Buffer{},
-		Slot:               piv.SlotCardAuthentication,
-		Prompt:             nil,
-	}
-	err = Sign(yk, signOpts)
-	assert.NoError(t, err)
 }
